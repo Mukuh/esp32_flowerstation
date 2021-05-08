51c37a987f7b5ea75ad0935fe46c6317d3fd93cd
Mukuh <tstelz@web.de>
2021-05-08 20:21:36 +0200

    [code] added bootstrap 5 and a scan html
    - navbar
    - scan.html
    - /scan

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import os
 wifi = Wifi()
 if settings.wifi_ssid is not None and settings.wifi_password is not None:
     # connect to wifi
+    networks = wifi.scan_wifi()
+    print(networks)
     wifi_status = wifi.connect(settings.wifi_ssid, settings.wifi_password)
 else:
     wifi_status = wifi.open_access_point(access_point_name=settings.access_point_name,
@@ -18,12 +20,26 @@ print("[INFO] wifi status %s" % wifi_status)
 
 app = picoweb.WebApp(__name__)
 
+try:
+    f = open("templates/welcome_html.py", "r")
+    f.close()
+    print("found templates/welcome_html.py")
+    print("deleting templates/welcome_html.py")
+    os.remove("templates/welcome_html.py")
+except OSError:
+    pass
+
 
 @app.route("/")
 def index(req, resp):
     yield from picoweb.start_response(resp)
-    # yield from resp.awrite("Hello world from picoweb running on the ESP32")
     yield from app.render_template(resp, "welcome.html", (req,))
+
+
+@app.route("/scan")
+def scan(req, resp):
+    yield from picoweb.start_response(resp)
+    yield from app.render_template(resp, "scan.html", (req,))
 
 
 app.run(debug=True, host="192.168.178.28")
